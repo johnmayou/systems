4b5e173cd04c64ec76d5ce958d75873169857c6d
John Mayou <john@johnmayou.com>
2026-06-21 18:19:45 -0500

    feat: start db and migrate before tests

--- bitly/scripts/ci-test.sh
+++ bitly/scripts/ci-test.sh
@@ -2,4 +2,9 @@
 
 set -euo pipefail
 
+docker compose -f docker-compose.test.yaml up -d --wait
+trap 'docker compose -f docker-compose.test.yaml down' EXIT
+
+goose -dir db/migrations postgres "postgres://postgres:postgres@localhost:5432/bitly_test" up
+
 go test -v ./... ${UPDATE:+-update}
