0ab1258baaf47e12ad9056d660861a32f7aa814b
John Mayou <john@johnmayou.com>
2026-06-21 18:01:40 -0500

    feat: test util for new test client + auto reset

---
+++ bitly/internal/testutil/db.go
@@ -0,0 +1,29 @@
+package testutil
+
+import (
+	"testing"
+
+	"github.com/johnmayou/systems/bitly/internal/dbx"
+	"github.com/stretchr/testify/require"
+)
+
+func NewTestDB(t *testing.T) *dbx.DB {
+	db, err := dbx.New(DatabaseURL())
+	require.NoError(t, err)
+	reset(t, db)
+	return db
+}
+
+func reset(t *testing.T, db *dbx.DB) {
+	err := db.Exec(`
+        DO $$ DECLARE
+            r RECORD;
+        BEGIN
+            FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = 'public' AND tablename != 'schema_migrations')
+            LOOP
+                EXECUTE 'TRUNCATE TABLE ' || quote_ident(r.tablename) || ' RESTART IDENTITY CASCADE';
+            END LOOP;
+        END $$;
+    `).Error
+	require.NoError(t, err)
+}
